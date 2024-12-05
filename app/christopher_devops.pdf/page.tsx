ce7c13ea6a2caaf531af485a002ca49b6c2d8d75
Christopher Egbaaibon <christopheregbaaibon@gmail.com>
2024-12-05 22:26:06 +0100

    Refactor PDF viewer component to use a more descriptive file extension

--- app/christopher_devops.pdf/page.tsx
+++ app/christopher_devops.pdf/page.tsx
@@ -5,12 +5,9 @@ import Navbar from '../components/navbar';
 const PdfViewer = () => {
   return (
     <div>
-      {/* Navbar */}
-      <Navbar />
-      {/* PDF Viewer */}
       <div className="container mx-auto mt-8">
         <iframe
-          src="/christopher_devops.pdf"
+          src="/christopher_devops.docx.pdf"
           width="100%"
           height="800px"
           className="border-2 border-gray-300"
